0c4e6ee3c5c764bd6d8cca31e628ca26c373c629
Nefers <steoxys42@gmail.com>
2020-12-07 12:12:45 +0100

    Choice with discord embeds

--- commands/Misc/lang.js
+++ commands/Misc/lang.js
@@ -1,28 +1,41 @@
-module.exports.run = (client) => {
+module.exports.run = (client, message) => {
 
   const commands = client.channels.cache.find(channel => channel.name === 'commands');
 
-  const rules2 = {
-    "title": "Liste des commandes",
-    "description": "```Voici les commandes disponibles sur le serveur.```",
+  const lang = {
+    "title": "Veuillez réagir pour choisir la langue du bot.",
     "color": 16556032,
     "fields": [
       {
-        "name": "Commandes générales :",
-        "value": "$help affiche la liste des commandes sur le serveur"
-      },
-      {
-        "name": "Commandes minecraft :",
-        "value": "$mc affiche les informations sur le serveur minecraft\n$rcon Envoie une commande sur le serveur minecraft"
-      },
-      {
-        "name": "Commandes Popsauce",
-        "value": "$join permet de rejoindre une partie popsauce\n$reset réinitialise la liste de joueurs\n$start lance la partie\n$rules affiche les règles du jeu\n$score affiche le tableau des scores.\n$skip passe à la prochaine image\n$stop stop la partie en cours\n$points permet de définir le nombre de points pour gagner la partie popsauce."
+        "name": "Langues disponibles.",
+        "value": "🇫🇷 Pour choisir le Français\n 🇬🇧 Pour choisir l'Anglais"
       }
     ]
   };
 
-  commands.send({embed : rules2}); //envoie l'embed contenant toute les informations dans le chat.
+commands.send({embed : lang}).then(async msg => {
+  await msg.react('🇫🇷');
+  await msg.react('🇬🇧');
+
+  const filter = (reaction, user) => {
+    return ['🇫🇷', '🇬🇧'].includes(reaction.emoji.name) && user.id === message.author.id;
+  };
+  
+  msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+    .then(collected => {
+      const reaction = collected.first();
+  
+      if (reaction.emoji.name === '🇫🇷') {
+        commands.send('La langue a été définie sur Français.');
+      } else {
+        commands.send('La langue a été définie sur Anglais.');
+      }
+    })
+    .catch(collected => {
+      message.reply('Tu as pris trop de temps à répondre.');
+    });
+
+})
 
 }
 
@@ -30,6 +43,6 @@ module.exports.help = {
   name:"language",
   description: "Change la langue",
   args: false,
-  admin: false,
+  admin: true,
   delete: false
 };
